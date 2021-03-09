cee3ec59323437e943de2b222883a69b37510cd2
yash <yashkumar201301@gmail.com>
2021-03-09 21:18:49 +0530

    calculate distance

--- app/src/main/java/com/example/distancetracker/util/MapUtil.kt
+++ app/src/main/java/com/example/distancetracker/util/MapUtil.kt
@@ -2,6 +2,8 @@ package com.example.distancetracker.util
 
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.SphericalUtil
+import java.text.DecimalFormat
 
 object MapUtil {
     fun setCameraPosition(location: LatLng): CameraPosition {
@@ -19,6 +21,16 @@ object MapUtil {
         val hours = (elapsedTime / (1000 * 60 * 60) % 24)
 
         return "$hours:$minutes:$seconds"
+    }
+
+    fun calculateTheDistance(locationList: MutableList<LatLng>): String {
+        if(locationList.size > 1){
+            val meters =
+                SphericalUtil.computeDistanceBetween(locationList.first(), locationList.last())
+            val kilometers = meters / 1000
+            return DecimalFormat("#.##").format(kilometers)
+        }
+        return "0.00"
     }
 
 }
